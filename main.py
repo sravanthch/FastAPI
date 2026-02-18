f6ef62fa176dc1f378575e323be6ac747d92ba6e
Sravanth Ch <sravanthch18@gmail.com>
2026-02-18 15:07:10 -0500

    Post Put Delete

--- main.py
+++ main.py
@@ -16,3 +16,34 @@ def greet():
 @app.get("/products")
 def get_all_products():
     return Products
+
+@app.get("/products/{id}")
+def getById(id:int):
+    for product in Products:
+        if product.id == id:
+            return product
+    return "Not Found"    
+
+
+@app.post("/product")
+def add_product(product: Product):
+    Products.append(product)
+    return product
+
+@app.put("/product/{id}")
+def update_product(id: int, product: Product):
+    for i in range(len(Products)):
+        if Products[i].id == id:
+            Products[i] = product
+            return "Product Updated Successfully"
+    return "No Product Found"    
+
+@app.delete("/product/{id}")
+def delete_product(id:int):
+    for i in range(len(Products)):
+        if Products[i].id == id:
+            del Products[i]
+            return "Product Deleted Successfully"
+    return "Failed To Delete Product"    
+
+
